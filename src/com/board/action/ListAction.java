d7a0e77f9ee877eb1a2fa4a04a5954dd72ec9d08
zoohwang <juhwang29@gmail.com>
2014-10-18 00:08:05 +0900

    add jstl lib

--- src/com/board/action/ListAction.java
+++ src/com/board/action/ListAction.java
@@ -4,6 +4,7 @@ import java.sql.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
+import java.util.List;
 
 import com.board.beans.Board;
 import com.board.controller.CommandAction;
@@ -18,7 +19,7 @@ public class ListAction implements CommandAction {
     public String requestPro(HttpServletRequest request,
                              HttpServletResponse reaponse) throws Throwable {
 
-        ArrayList<Board> articleList = BoardDao.getInstance().getArticleList();
+        List<Board> articleList = BoardDao.getInstance().selectAll();
 
         request.setAttribute("articleList", articleList);
 
--- src/com/board/dao/BoardDao.java
+++ src/com/board/dao/BoardDao.java
@@ -4,6 +4,7 @@ import java.beans.Statement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 import com.board.beans.Board;
 
@@ -19,32 +20,45 @@ public class BoardDao extends CommonDao {
         return _instance;
     }
 
-    public ArrayList<Board> getArticleList() throws SQLException {
+    public List<Board> selectAll() {
+        List<Board> list = null;
 
-        ResultSet rs = null;
-
-        String sql = "select * from BOARD order by idx desc";
-
-        rs = openConnection().executeQuery(sql); // sql을 실행하기위해 연결을 열어 쿼리를 실행하고
-
-        // rs에 반환합니다.
-
-        ArrayList<Board> articleList = new ArrayList<Board>();//  Board형 배열객체를 선언
-
-        while (rs.next()) {
-            Board article = new Board();// 데이터들을 담기위해 Board객체에 메모리를 할당
-
-            article.setIdx(rs.getInt("idx"));
-            article.setTitle(rs.getString("title"));
-            article.setWriter(rs.getString("writer"));
-            article.setRegdate(rs.getString("regdate"));
-
-            articleList.add(article);// 셋팅된 빈을 리스트에 추가합니다.
+        try {
+            list = session.selectList("Board.selectAll");
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
         }
-
-        closeConnection(); // 연결을 꼭 닫아줍시다.
-
-        return articleList;
-
+        return list;
     }
+
+//    public ArrayList<Board> getArticleList() throws SQLException {
+//
+//        ResultSet rs = null;
+//
+//        String sql = "select * from BOARD order by idx desc";
+//
+//        rs = openConnection().executeQuery(sql); // sql을 실행하기위해 연결을 열어 쿼리를 실행하고
+//
+//        // rs에 반환합니다.
+//
+//        ArrayList<Board> articleList = new ArrayList<Board>();//  Board형 배열객체를 선언
+//
+//        while (rs.next()) {
+//            Board article = new Board();// 데이터들을 담기위해 Board객체에 메모리를 할당
+//
+//            article.setIdx(rs.getInt("idx"));
+//            article.setTitle(rs.getString("title"));
+//            article.setWriter(rs.getString("writer"));
+//            article.setRegdate(rs.getString("regdate"));
+//
+//            articleList.add(article);// 셋팅된 빈을 리스트에 추가합니다.
+//        }
+//
+//        closeConnection(); // 연결을 꼭 닫아줍시다.
+//
+//        return articleList;
+//
+//    }
 }
