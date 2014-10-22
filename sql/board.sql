905b164fe52dac29f304f155ed9b6047f6a28093
zoohwang <juhwang29@gmail.com>
2014-10-22 15:40:23 +0900

    add sql

---
+++ sql/board.sql
@@ -0,0 +1,13 @@
+create table board
+(
+     idx int(10) NOT NULL AUTO_INCREMENT,
+     title varchar(200) not null,
+     writer varchar(20) not null,
+     regdate varchar(20) not null,
+     count int(10),
+     content MEDIUMTEXT,
+    PRIMARY KEY(idx)
+);
+
+나중에 추가.
+alter table board add regip varchar(20);
